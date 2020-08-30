cf8274b54694ab217f31fdd746317f8648921b8d
Grant Pardington <grant.pardington@gmail.com>
2020-08-30 13:06:53 -0600

    Added code to intern.js file

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,16 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+//Define and export the Intern class. Inherit from Employee.
+class intern {
+    constructor(name, id, email, gitHubUserName) {
+        super(name, id, email);
+        this.school = school;
+        this.role = ("Intern");
+    }
+    getGitHub() {
+        return (this.gitHub);
+    }
+
+    getRole() {
+        return (this.role);
+    }
+}
+module.exports = intern;
